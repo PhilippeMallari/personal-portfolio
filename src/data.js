fee21e0fe968be19f7e10ab6340b6f4671112a1d
PhilippeMallari <mallari.philippe@gmail.com>
2023-03-16 23:01:56 -0600

    finished projects.js | updated UI

--- src/data.js
+++ src/data.js
@@ -1,54 +1,66 @@
 export const projects = [
     {
-        title: "React Reserve",
-        subtitle: "MERN Stack",
-        description: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laborum quisquam alias fuga saepe quos tempora? Animi ipsa vitae est velit.",
-        image: "./project-1.gif",
-        link: "https://reactbootcamp.com"
-    },
-    {
-        title: "React Tracks",
-        subtitle: "React and Python",
+        id: 1,
+        title: "Feedback App",
+        subtitle: "React JS",
         description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-2.gif",
-        link: "https://reedbarger.com",
-    },
-    {
-        title: "DevChat",
-        subtitle: "React and Firebase",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-3.gif",
-        link: "https://jsbootcamp.com",
-    },
-    {
-        title: "Epic Todo App",
-        subtitle: "React Hooks",
-        description:
-            "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
-        image: "./project-4.gif",
-        link: "https://pythonbootcamp.com",
-    },
-]
+            "A simple feedback app that utilizes React JS fundamentals like Components, Props, State.",
+        image: "./images/feedbackapp.gif",
+        link: "https://reactbootcamp.com",
+    }
+    // },
+    // {
+    //     id: 2,
+    //     title: "React Tracks",
+    //     subtitle: "React and Python",
+    //     description:
+    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //     image: "./images/logo-2.PNG",
+    //     link: "https://reedbarger.com",
+    // },
+    // {
+    //     id: 3,
+    //     title: "DevChat",
+    //     subtitle: "React and Firebase",
+    //     description:
+    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //     image: "./images/logo-3.PNG",
+    //     link: "https://jsbootcamp.com",
+    // },
+    // {
+    //     id: 4,
+    //     title: "Epic Todo App",
+    //     subtitle: "React Hooks",
+    //     description:
+    //         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+    //     image: "./images/logo-4.PNG",
+    //     link: "https://pythonbootcamp.com",
+    // },
+];
 
 export const skills = [
     {
+        id: 1,
         technology: "JavaScript"
     },
     {
+        id: 2,
         technology: "React JS"
     },
     {
+        id: 3,
         technology: "Node JS"
     },
     {
+        id: 4,
         technology: "Mongo DB"
     },
     {
+        id: 5,
         technology: "Express JS"
     },
     {
+        id: 6,
         technology: "Tailwind CSS"
     }
 ]
